983b1e7520db182c35379e8908055d23d130937b
gyjiang16 <115113274+gyjiang16@users.noreply.github.com>
2024-10-27 12:29:14 -0400

    Update readme.m

--- SLAM data/readme.m
+++ SLAM data/readme.m
@@ -1,1 +1,2 @@
-
+# SLAM dataset
+# This dataset is created for SLAM testing and includes two cameras (a regular frame-based camera and an event-based Dynamical Visual Sensor (DVS) camera) simultaneously capturing multiple indoor and outdoor scenarios. The indoor dataset features different floors of a campus building, while the outdoor dataset was recorded around the campus.
