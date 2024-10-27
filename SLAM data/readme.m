0b40b2f6f3ba189d6a7e282a28efb5940e036824
gyjiang16 <115113274+gyjiang16@users.noreply.github.com>
2024-10-27 12:30:57 -0400

    Update readme.m

--- SLAM data/readme.m
+++ SLAM data/readme.m
@@ -1,2 +1,7 @@
 # SLAM dataset
 # This dataset is created for SLAM testing and includes two cameras (a regular frame-based camera and an event-based Dynamical Visual Sensor (DVS) camera) simultaneously capturing multiple indoor and outdoor scenarios. The indoor dataset features different floors of a campus building, while the outdoor dataset was recorded around the campus.
+
+
+# This dataset is about 10GB large, pls contact me for dataset.
+# My email address: gjiang@students.kennesaw.edu
+
