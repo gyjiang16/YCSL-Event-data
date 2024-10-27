e2b06b9c5d0443aefe99d4237ce18412a5df9b1e
gyjiang16 <115113274+gyjiang16@users.noreply.github.com>
2024-10-27 12:27:52 -0400

    Update and rename read.m to readme.m

---
+++ diff-objects/readme.m
@@ -0,0 +1,2 @@
+#diff-objects dataset
+This dataset involves the movement of five different shaped objects, individually or in groups, undergoing four types of motion: swinging, rolling, throwing, and colliding.
